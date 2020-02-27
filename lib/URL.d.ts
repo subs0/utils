9c2be5e742969800db49fe184eebd2afd5c50502
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 12:38:38 -0500

    switching to subtree directory and running updates

--- lib/URL.d.ts
+++ lib/URL.d.ts
@@ -1,10 +1,10 @@
 import { URL_FULL, URL_SUBD, URL_DOMN, URL_QERY, URL_HASH, URL_PATH } from "@-0/keys";
 export declare const parse: (URL_full: string, prefixRGX?: any) => {
-    [URL_FULL]: string;
-    [URL_SUBD]: any[];
-    [URL_DOMN]: any[];
-    [URL_PATH]: any[];
-    [URL_QERY]: any;
-    [URL_HASH]: string;
+    URL: string;
+    URL_subdomain: any[];
+    URL_domain: any[];
+    URL_path: any[];
+    URL_query: any;
+    URL_hash: string;
 };
 export declare const unparse: (parsed: any, isAbsolute?: boolean) => string;
