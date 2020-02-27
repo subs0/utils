f8389d85bef3bb9ca0e007ae38be612b37614832
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 14:26:50 -0500

    switching to subtree directory and running updates

---
+++ lib/taskDelay.d.ts
@@ -0,0 +1,1 @@
+export declare const msTaskPromiseDelay: (ms: any) => Promise<unknown>;
---
+++ lib/xKey.d.ts
@@ -0,0 +1,3 @@
+export declare const stringify_w_functions: (x: any, indent?: any) => string;
+export declare const key_index_err: (c: any, i: any) => string;
+export declare const xKeyError: (str: any, c: any, unknown: any, sub$: any, index: any) => string;
