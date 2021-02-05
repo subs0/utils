a9988b7197b2e75152ceedb039d3f640bce3eec1
Logan Powell <loganp@tepper.cmu.edu>
2021-02-05 06:29:03 -0500

    switching to subtree directory and running updates

--- lib/stringify_type.d.ts
+++ lib/stringify_type.d.ts
@@ -1,1 +1,1 @@
-export declare const stringify_type: (x: any) => "NULLARY" | "UNARY" | "BINARY" | "N-ARY" | "PROMISE" | "ARRAY" | "OBJECT" | "ERROR" | "PRIMITIVE" | "UNDEFINED";
+export declare const stringify_type: (x: any) => "NULLARY" | "UNARY" | "BINARY" | "N-ARY" | "PROMISE" | "ARRAY" | "OBJECT" | "ERROR" | "PRIMITIVE" | "UNKNOWN";
--- lib/xKey.d.ts
+++ lib/xKey.d.ts
@@ -1,2 +1,2 @@
 export declare const key_index_err: (c: any, i: any) => string;
-export declare const xKeyError: (str?: string, c?: {}, unknown_keys?: any[], index?: number) => string;
+export declare const xKeyError: (err_str?: string, c?: {}, unknown_keys?: any[], index?: number) => string;
