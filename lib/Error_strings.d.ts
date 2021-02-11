f8d9b13686b670c8959c017b776824afa67719d7
Logan Powell <loganp@tepper.cmu.edu>
2021-02-11 16:10:45 -0500

    switching to subtree directory and running updates

--- lib/Error_strings.d.ts
+++ lib/Error_strings.d.ts
@@ -1,5 +1,2 @@
 export declare const missing_prop_list: (obj?: {}) => string[];
-export declare const Err_missing_props: (CMD?: string, obj?: {
-    sub$: any;
-    args: any;
-}) => string;
+export declare const Err_missing_props: (CMD?: string, obj?: {}) => string;
