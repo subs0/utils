87ad55dcbf6ff036292b00707acc881acdba36be
Logan Powell <loganp@tepper.cmu.edu>
2021-02-13 08:15:33 -0500

    syncing supertree with subtree

--- src/Error_strings.ts
+++ src/Error_strings.ts
@@ -1,6 +1,6 @@
 import { CMD_SUB$, CMD_ARGS, CMD_WORK, CMD_ERRO, CMD_RESO } from "@-0/keys"
 
-const missing_props_str = (CMD = "", ...props) => {
+const missing_props_str = (CMD = "", args, ...props) => {
     const missing = props.map(x => `    ${x}: 🔥`).join(",\n")
     return `
 Error: ${CMD} Command missing critical \`${CMD_ARGS}\`:
@@ -10,7 +10,10 @@ Error: ${CMD} Command missing critical \`${CMD_ARGS}\`:
 ${missing}
   }
 }
-This Command's registered \`${CMD_WORK}\` handler failed 
+This Command's registered \`${CMD_WORK}\` handler failed.
+
+Here were the supplied \`${CMD_ARGS}\`:
+${args}
     `
 }
 
@@ -18,6 +21,6 @@ export const missing_prop_list = (obj = {}) => {
     return Object.entries(obj).map(([ k, v ]) => (v ? k : null)).filter(x => x !== null)
 }
 
-export const Err_missing_props = (CMD = "", obj = {}) => {
-    return missing_props_str(CMD, ...missing_prop_list(obj))
+export const Err_missing_props = (CMD = "", obj = {}, args = {}) => {
+    return missing_props_str(CMD, args, ...missing_prop_list(obj))
 }
