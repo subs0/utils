da150cc341989e1231429f377fbc74a1479d6a5e
Logan Powell <loganp@tepper.cmu.edu>
2021-02-16 16:36:13 -0500

    syncing supertree with subtree

--- src/Error_strings.ts
+++ src/Error_strings.ts
@@ -15,15 +15,18 @@ ${missing}
 
 This Command's registered \`${CMD_WORK}\` handler failed.
 
-Here were the supplied \`${CMD_ARGS}\`:
-${stringify_fn(args, 2)}
+The Command as supplied:
+{ ...,
+  ${CMD_SUB$}: ${CMD},
+  ${CMD_ARGS}: ${stringify_fn(args, 2)}
+}
     `
 }
 
-export const missing_prop_list = (obj = {}) => {
-    return Object.entries(obj).map(([ k, v ]) => (v ? k : null)).filter(x => x !== null)
+export const missing_prop_list = (props = {}) => {
+    return Object.entries(props).map(([ k, v ]) => (v ? null : k)).filter(x => x !== null)
 }
 
-export const Err_missing_props = (CMD = "", obj = {}, args = {}) => {
-    return missing_props_str(CMD, args, ...missing_prop_list(obj))
+export const Err_missing_props = (CMD = "", props = {}, args = {}) => {
+    return missing_props_str(CMD, args, ...missing_prop_list(props))
 }
