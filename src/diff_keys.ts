2c67ff89e11ef28f15500ec58b3a351793a4bf4a
Logan Powell <loganp@tepper.cmu.edu>
2021-08-25 14:59:58 -0400

    updates package version and syncs subtree

--- src/diff_keys.ts
+++ src/diff_keys.ts
@@ -12,7 +12,7 @@
  * diff_keys(["a", "b"], { a: 1, b: 2, c: 3, d: 4 })
  * //=> [ [ 'c', 'd' ], { c: 3, d: 4 } ]
  */
-export function diff_keys(nKeys = [], nObj = {}) {
+export const diff_keys = (nKeys = [], nObj = {}) => {
     const all = Object.keys(nObj)
     const xKeys = all.filter(key => !nKeys.includes(key))
     const xObj = Object.entries(nObj).reduce((a, [k, v]) => {
